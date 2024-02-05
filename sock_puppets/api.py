07c07b3f9ad658b3f7393673d4b2487829d1a4cd
Muhammad Haroon <m_haroon96@hotmail.com>
2024-02-04 20:56:53 -0800

    moved out build func

--- sock_puppets/api.py
+++ sock_puppets/api.py
@@ -3,12 +3,19 @@ import json
 
 IMAGE_NAME = 'mharoon/fuzzer'
 
-def api(training, script):
+def build():
     client = docker.from_env()
     # build image
-    client.images.build(path='./sock_puppets', tag=IMAGE_NAME, rm=True)
+    client.images.build(path='.', tag=IMAGE_NAME, rm=True)
+    
+def api(training, script):
+    client = docker.from_env()
     # start container
     command = ['python', script, json.dumps(training)]
     response = client.containers.run(IMAGE_NAME, command, shm_size='512M', remove=True)
+    print(response)
     response = json.loads(response)
     return response
+
+if __name__ == '__main__':
+    build()
