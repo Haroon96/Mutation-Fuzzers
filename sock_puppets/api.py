7271b2cd65636aef90c5aae028ee63c028fc842a
Muhammad Haroon <m_haroon96@hotmail.com>
2024-02-04 20:46:20 -0800

    bug fixes

--- sock_puppets/api.py
+++ sock_puppets/api.py
@@ -9,6 +9,7 @@ def api(training, script):
     client.images.build(path='./sock_puppets', tag=IMAGE_NAME, rm=True)
     # start container
     command = ['python', script, json.dumps(training)]
-    response = client.containers.run(IMAGE_NAME, command, shm_size='512M', remove=True)
-    response = json.loads(response)
-    return response
+    stdout = client.containers.run(IMAGE_NAME, command, shm_size='512M', remove=True).decode()
+    data = stdout.strip().split('\n')[-1]
+    data = json.loads(data)
+    return data
