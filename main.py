020ecb7f3d841b946fd9eb5519df2b213315ce4c
Muhammad Haroon <m_haroon96@hotmail.com>
2024-02-07 00:53:48 -0800

    added resumption and server based cataloging

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import requests
 import json
 from strategies import strategies, Strategy
 import sock_puppets.api as env
-from data_generators.data_generator import DataGenerator
 from data_generators.youtube_data_generator import YouTubeVideoGenerator
+from socket import gethostname
 
 # default directories
 DATA_DIR = os.path.join(os.getcwd(), 'data')
@@ -14,9 +14,13 @@ DATA_DIR = os.path.join(os.getcwd(), 'data')
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('--strategy', required=True, choices=list(strategies))
-    parser.add_argument('--num-videos', default=30)
+    parser.add_argument('--num-videos', default=30, type=int)
     parser.add_argument('--alpha', default=0.1, type=float)
-    parser.add_argument('--generations', default=100)
+    parser.add_argument('--generations', default=100, type=int)
+    parser.add_argument('--initial-trace')
+    parser.add_argument('--initial-generation', default=0, type=int)
+    parser.add_argument('--run-id')
+    parser.add_argument('--is-resume', action='store_true')
     return parser.parse_args()
 
 def main(args):
@@ -27,14 +31,18 @@ def main(args):
     # pick strategy
     strategy: Strategy = strategies[args.strategy](data_gen)
 
-    # generate run id
-    run_id = str(uuid4())[:8]
+    # check if resuming an older run or starting new
+    if args.is_resume:
+        run_id = args.run_id
+        trace = json.loads(args.initial_trace)
+        trace = strategy.mutate(trace, args.alpha)
+        generation = args.initial_generation + 1
+    else:
+        run_id = str(uuid4())[:8]
+        trace = data_gen.sample_videos(args.num_videos)
+        generation = 0
 
-    # generate initial trace
-    trace = data_gen.sample_videos(args.num_videos)
-
-    # run until convergence
-    generation = 0
+    # run until hit num of generations
     while generation < args.generations:
 
         # run trace
@@ -47,12 +55,13 @@ def main(args):
             strategy=args.strategy,
             alpha=args.alpha,
             num_videos=args.num_videos,
+            generations=args.generations,
             training=trace,
             recommendations=recommendations
         )
 
         # send data to server
-        requests.post('http://lake.cs.ucdavis.edu/fuzzerapi/process-data', data=json.dumps(post_data))
+        requests.post(f'http://lake.cs.ucdavis.edu/fuzzerapi/process-data/{gethostname()}', data=json.dumps(post_data))
 
         # get new trace
         trace = strategy.mutate(trace, args.alpha)
---
+++ resume.py
@@ -0,0 +1,60 @@
+import subprocess
+from argparse import ArgumentParser
+import sys
+from time import sleep
+from random import randint
+import requests
+from socket import gethostname
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('-mp', '--max-parallel', type=int, default=5)
+    parser.add_argument('-wt', '--wait-time', type=float, default=5)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    processes = []
+    
+    r = requests.get(f'http://lake.cs.ucdavis.edu/fuzzerapi/unfinished-runs/{gethostname()}')
+    unfinished_runs = r.json()
+    
+    for run in unfinished_runs:
+        # sleep if max processes
+        while len(processes) >= args.max_parallel:
+            sleep(args.wait_time * 60)
+            
+            # find and remove finished processes
+            for process in processes:
+                if process.poll() is not None:
+                    processes.remove(process)
+        
+        # spawn process
+        run_id = run['run_id']
+        strategy = run['strategy']
+        alpha = str(run['alpha'])
+        num_videos = str(run['num_videos'])
+        trace = run['training']
+        generations = str(run['generations'])
+        generation = str(run['generation'])
+        
+        print("Resuming", run_id, strategy, alpha)
+        proc = subprocess.Popen([sys.executable, 'main.py', 
+                                 '--is-resume',
+                                 '--run-id', run_id,
+                                 '--strategy', strategy, 
+                                 '--alpha', alpha, 
+                                 '--num-videos', num_videos,
+                                 '--generations', generations,
+                                 '--initial-trace', trace,
+                                 '--initial-generation', generation
+                                 ])
+        processes.append(proc)
+        sleep(randint(1, 5))
+
+    # wait for processes to finish
+    while len(processes) > 0:
+        for process in processes:
+            if process.poll() is not None:
+                processes.remove(process)
+        sleep(args.wait_time * 60)
