dab2422811c7eb63109106e7c2a24dca489b5648
Muhammad Haroon <m_haroon96@hotmail.com>
2024-02-14 01:17:56 -0800

    added print

--- resume.py
+++ resume.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     
     r = requests.get(f'http://lake.cs.ucdavis.edu/fuzzerapi/unfinished-runs/{gethostname()}')
     unfinished_runs = r.json()
+    print("# of unfinished runs", len(unfinished_runs))
     
     for run in unfinished_runs:
         # sleep if max processes
