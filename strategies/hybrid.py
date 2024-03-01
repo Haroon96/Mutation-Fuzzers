999a8e5c8afed88bcd23bf203293eff0129e3e08
Muhammad Haroon <m_haroon96@hotmail.com>
2024-02-29 19:05:38 -0800

    added eps param

--- strategies/hybrid.py
+++ strategies/hybrid.py
@@ -5,12 +5,13 @@ from random import choices, random
 class Hybrid(Strategy):
     def __init__(self, data_gen):
         self.data_gen:DataGenerator = data_gen
+        self.epsilon = 0.5
 
     def mutate(self, trace, alpha):
         mutated_trace = trace[:]
         n = len(mutated_trace)
         ss = 1 if alpha is None else int(n * alpha)
-        if random() <= 0.8:
+        if random() <= self.epsilon:
             sample = self.data_gen.sample_bugs(ss)
         else:
             sample = self.data_gen.sample_videos(ss)
