91c59a3c15f30cf7a868a0e491dd7fc308e07d0b
Muhammad Haroon <m_haroon96@hotmail.com>
2024-03-07 17:15:45 -0800

    changed resume strat

--- resume.py
+++ resume.py
@@ -16,51 +16,53 @@ if __name__ == '__main__':
     args = parse_args()
     processes = []
     
-    r = requests.get(f'http://lake.cs.ucdavis.edu/fuzzerapi/unfinished-runs/{gethostname()}')
-    unfinished_runs = r.json()
-    unfinished_runs = [i for i in unfinished_runs if i['alpha'] is not None and float(i['alpha']) == 0.5]
-    print("# of unfinished runs", len(unfinished_runs))
-    
-    for run in unfinished_runs:
-        # sleep if max processes
-        while len(processes) >= args.max_parallel:
-            sleep(args.wait_time * 60)
+
+    while True:
+        r = requests.get(f'http://lake.cs.ucdavis.edu/fuzzerapi/unfinished-runs/{gethostname()}')
+        unfinished_runs = r.json()
+        unfinished_runs = [i for i in unfinished_runs if i['alpha'] is not None and float(i['alpha']) == 0.5]
+        print("# of unfinished runs", len(unfinished_runs))
+        
+        for run in unfinished_runs:
+            # sleep if max processes
+            while len(processes) >= args.max_parallel:
+                sleep(args.wait_time * 60)
+                
+                # find and remove finished processes
+                for process in processes:
+                    if process.poll() is not None:
+                        processes.remove(process)
             
-            # find and remove finished processes
+            # spawn process
+            run_id = run['run_id']
+            strategy = run['strategy']
+            alpha = run['alpha']
+            num_videos = str(run['num_videos'])
+            trace = run['training']
+            generations = str(run['generations'])
+            generation = str(run['generation'])
+            
+            resume_args = [
+                '--is-resume',
+                '--run-id', run_id,
+                '--strategy', strategy, 
+                '--num-videos', num_videos,
+                '--generations', generations,
+                '--initial-trace', trace,
+                '--initial-generation', generation
+            ]
+            if alpha is not None:
+                resume_args.extend(['--alpha', str(alpha)])
+
+            print("Resuming", run_id, strategy, alpha, num_videos, f'{generation}/{generations}')
+            proc = subprocess.Popen([sys.executable, 'concurrent-main.py', *resume_args])
+            processes.append(proc)
+            sleep(randint(1, 15))
+
+        # wait for processes to finish
+        print('waiting for processes to end')
+        while len(processes) > 0:
             for process in processes:
                 if process.poll() is not None:
                     processes.remove(process)
-        
-        # spawn process
-        run_id = run['run_id']
-        strategy = run['strategy']
-        alpha = run['alpha']
-        num_videos = str(run['num_videos'])
-        trace = run['training']
-        generations = str(run['generations'])
-        generation = str(run['generation'])
-        
-        resume_args = [
-            '--is-resume',
-            '--run-id', run_id,
-            '--strategy', strategy, 
-            '--num-videos', num_videos,
-            '--generations', generations,
-            '--initial-trace', trace,
-            '--initial-generation', generation
-        ]
-        if alpha is not None:
-            resume_args.extend(['--alpha', str(alpha)])
-
-        print("Resuming", run_id, strategy, alpha, num_videos, f'{generation}/{generations}')
-        proc = subprocess.Popen([sys.executable, 'main.py', *resume_args])
-        processes.append(proc)
-        sleep(randint(1, 15))
-
-    # wait for processes to finish
-    print('waiting for processes to end')
-    while len(processes) > 0:
-        for process in processes:
-            if process.poll() is not None:
-                processes.remove(process)
-        sleep(args.wait_time * 60)
+            sleep(args.wait_time * 60)
